499aaf13a750b439ef92dc90f35dc792dd00125f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-12 16:20:29 +0100

    Updated multi-DE simulation for multi-core execution.

--- simulations/sim_multiDE.R
+++ simulations/sim_multiDE.R
@@ -1,84 +1,90 @@
-# This checks the brittleness of the summation approach in the presence of DE genes.
+# This checks the performance of normalization methods in the presence of multiple subpopulations. 
+# First we define a function that simulates the data and evaluates each method.
 
-source("functions.R")
-dir.out <- "results_multiDE"
-dir.create(dir.out, showWarnings=FALSE)
-fout <- file.path(dir.out, "summary.txt")
-is.there <- FALSE 
+FUN <- function(mode, overlap, genes.up, genes.down, prop, effect, dir.out) {
+    source("functions.R")
 
-for (mode in c("UMI", "read")) { 
-    for (overlap in c(TRUE, FALSE)) {
-        cur.dir <- file.path(dir.out, paste0(mode, "-", overlap))
-        dir.create(cur.dir, showWarnings=FALSE)
+    # Skipping impossible scenarios.
+    if (genes.up==0 && genes.down==0) {
+        return(NULL)
+    } else if (genes.up==0 && effect!=2) {
+        return(NULL)
+    }
+    
+    prefix <- sprintf("%s-%s-u%i-d%i-p%.1f-e%i", mode, overlap, genes.up, genes.down, prop, effect)
+    stub <- file.path(dir.out, prefix)
+    fout <- paste0(stub, ".tsv")
+
+    for (it in 1:10) { 
         ncells <- 1000
+        param <- generateRawMeans(ncells=ncells, mode=mode)
+        truth <- param$sf
 
-        for (genes.up in c(0, 1000, 2000)) {
-            for (genes.down in c(0, 1000, 2000)) {
-                if (genes.up==0){
-                    if (genes.down==0) { next; }
-                    effects <- 1
-                } else {
-                    effects <- c(2, 5, 10)
-                }
+        # Adding the requested DE (second population has reverse profile from the first population).
+        mus <- param$fitted
+    
+        de.cell1 <- seq_len(ncells*prop)
+        goes.up1 <- seq_len(genes.up)
+        goes.down1 <- seq_len(genes.down) + genes.up
+        mus[goes.up1,de.cell1] <- mus[goes.up1,de.cell1] * effect
+        mus[goes.down1,de.cell1] <- 0
+    
+        de.cell2 <- seq_len(ncells*prop) + max(de.cell1)
+        goes.up2 <- seq_len(genes.down) 
+        if (!overlap) { # Do the DE genes in the second population overlap with the first?
+            goes.up2 <- goes.up2 + max(goes.down1)
+        }
+        goes.down2 <- seq_len(genes.up) + max(goes.up2)
+        mus[goes.up2,de.cell2] <- mus[goes.up2,de.cell2] * effect
+        mus[goes.down2,de.cell2] <- 0
+    
+        de.cell <- c(de.cell1, de.cell2)
+        counts <- sampleCounts(mus, param$dispersion)
+        output <- runAllMethods(counts)
 
-                for (prop in c(0.1, 0.2, 0.3)) {
-                    for (effect in effects) {
+        # Assessing the different methods. 
+        collected <- vector("list", length(output))
+        names(collected) <- names(output)
+        if (it==1L) {
+            # Generating an output plot for this simulation.
+            col <- rep("black", ncells)
+            col[de.cell1] <- "orange"
+            col[de.cell2] <- "dodgerblue"
 
-###### BROKEN INDENTING ######
-
-    stub <- file.path(cur.dir, sprintf("u%i-d%i-p%.1f-e%i", genes.up, genes.down, prop, effect))
-    param <- generateRawMeans(ncells=ncells, mode=mode)
-    truth <- param$sf
-
-    # Adding the requested DE (second population has reverse profile from the first population).
-    mus <- param$fitted
-
-    de.cell1 <- seq_len(ncells*prop)
-    goes.up1 <- seq_len(genes.up)
-    goes.down1 <- seq_len(genes.down) + genes.up
-    mus[goes.up1,de.cell1] <- mus[goes.up1,de.cell1] * effect
-    mus[goes.down1,de.cell1] <- 0
-
-    de.cell2 <- seq_len(ncells*prop) + max(de.cell1)
-    goes.up2 <- seq_len(genes.down) 
-    if (!overlap) { # Do the DE genes in the second population overlap with the first?
-        goes.up2 <- goes.up2 + max(goes.down1)
-    }
-    goes.down2 <- seq_len(genes.up) + max(goes.up2)
-    mus[goes.up2,de.cell2] <- mus[goes.up2,de.cell2] * effect
-    mus[goes.down2,de.cell2] <- 0
-
-    de.cell <- c(de.cell1, de.cell2)
-    counts <- sampleCounts(mus, param$dispersion)
-    output <- runAllMethods(counts)
-
-    # Generating an output plot for this simulation.
-    collected <- vector("list", length(output))
-    names(collected) <- names(output)
-    col <- rep("black", ncells)
-    col[de.cell1] <- "orange"
-    col[de.cell2] <- "dodgerblue"
-
-    pdf(paste0(stub, ".pdf"))
-    par(mar=c(5.1,5.1,4.1,1.1))
-    for (x in names(output)) {
-        collected[[x]] <- makeSFPlot(output[[x]], truth, de.cell, main=x, col=col)["DE"]
-    }
-    dev.off()
-
-    # Writing summary statistics to table.
-    stats <- do.call(cbind, collected)
-    write.table(file=fout, data.frame(Mode=mode, Overlap=overlap, Ncells=ncells, Up=genes.up, Down=genes.down, 
-                                      Prop=prop, Effect=effect, format(stats, digits=3)),
-                append=is.there, quote=FALSE, sep="\t", col.names=!is.there, row.names=FALSE)
-    is.there <- TRUE
-
-###### END INDENTING ######
-
-                    }
-                }
+            pdf(paste0(stub, ".pdf"))
+            par(mar=c(5.1,5.1,4.1,1.1))
+            for (x in names(output)) {
+                collected[[x]] <- evaluateSizeFactors(output[[x]], truth, de.cell, main=x, col=col)["DE"]
+            }
+            dev.off()
+        } else {
+            for (x in names(output)) {
+                collected[[x]] <- evaluateSizeFactors(output[[x]], truth, de.cell, plot=FALSE)["DE"]
             }
         }
-    }       
+
+        # Writing summary statistics to table.
+        stats <- do.call(cbind, collected)
+        write.table(file=fout, data.frame(format(stats, digits=3)),
+                append=it!=1, quote=FALSE, sep="\t", col.names=it==1, row.names=FALSE)
+    }
+    return(NULL)
 }
 
+##############################################
+
+source("functions.R")
+all.scenarios <- spawnScenarios(
+    mode=c("UMI", "read"), 
+    overlap=c(TRUE, FALSE),
+    genes.up=c(0, 1000, 2000), 
+    genes.down=c(0, 1000, 2000),
+    effect=c(2, 5, 10),
+    prop=c(0.1, 0.2, 0.3))
+
+dir.out <- "results_multiDE"
+dir.create(dir.out, showWarnings=FALSE)
+
+library(BiocParallel)
+BPPARAM <- createBatchParam(20)
+X <- do.call(bpmapply, c(list(FUN=FUN, SIMPLIFY=FALSE, USE.NAMES=FALSE, BPPARAM=BPPARAM, MoreArgs=list(dir.out=dir.out)), all.scenarios))
