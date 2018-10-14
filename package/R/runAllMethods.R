3b911de7328c6d9f3044dcdcf9b484c0f6d6fc4f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-14 12:19:06 +0100

    Removed EOL whitespace, fixed indenting.

--- package/R/runAllMethods.R
+++ package/R/runAllMethods.R
@@ -13,7 +13,7 @@
 #' \item DESeq normalization with \code{\link{estimateSizeFactorsForMatrix}}, where all zeroes are ignored during calculation of the geometric mean.
 #' \item DESeq normalization using the arithmetic mean.
 #' \item DESeq normalization with an added pseudo-count of 1.
-#' \item DESeq normalization with a library size-adjusted pseudo-count 
+#' \item DESeq normalization with a library size-adjusted pseudo-count
 #' \item Library size normalization with \code{\link{librarySizeFactors}}.
 #' \item Deconvolution with \code{\link{computeSumFactors}} and without any pre-clustering.
 #' \item Deconvolution with \code{\link{computeSumFactors}} and pre-clustering with \code{\link{quickCluster}}.
@@ -39,54 +39,54 @@
 #' out <- runAllMethods(counts)
 #' str(out)
 runAllMethods <- function(counts, threshold=0) {
-{
-    subcounts <- counts[calcAverage(counts) > threshold,]
-    subcounts <- as.matrix(subcounts)
+    {
+        subcounts <- counts[calcAverage(counts) > threshold,]
+        subcounts <- as.matrix(subcounts)
 
-    # TMM with raw counts:
-    tmm.sf <- calcNormFactors(subcounts) * colSums(subcounts)
+        # TMM with raw counts:
+        tmm.sf <- calcNormFactors(subcounts) * colSums(subcounts)
 
-    # TMM with averaged counts:
-    combined <- cbind(rowSums(subcounts), subcounts)
-    tmm2.sf <- calcNormFactors(combined, refColumn=1) * colSums(combined)
-    tmm2.sf <- tmm2.sf[-1]
+        # TMM with averaged counts:
+        combined <- cbind(rowSums(subcounts), subcounts)
+        tmm2.sf <- calcNormFactors(combined, refColumn=1) * colSums(combined)
+        tmm2.sf <- tmm2.sf[-1]
 
-    # Size factors with raw counts (must counter zeros for both geometric mean and for each library):
-    logvals <- log(subcounts)
-    logvals[is.infinite(logvals)] <- NA_real_
-    gm <- exp(rowMeans(logvals, na.rm=TRUE))
-    size.sf <- estimateSizeFactorsForMatrix(subcounts, geoMeans=gm)
+        # Size factors with raw counts (must counter zeros for both geometric mean and for each library):
+        logvals <- log(subcounts)
+        logvals[is.infinite(logvals)] <- NA_real_
+        gm <- exp(rowMeans(logvals, na.rm=TRUE))
+        size.sf <- estimateSizeFactorsForMatrix(subcounts, geoMeans=gm)
 
-    # Size factors with averaged counts (still removes zeros in each library):
-    size2.sf <- estimateSizeFactorsForMatrix(subcounts, geoMeans=rowMeans(subcounts))
+        # Size factors with averaged counts (still removes zeros in each library):
+        size2.sf <- estimateSizeFactorsForMatrix(subcounts, geoMeans=rowMeans(subcounts))
 
-    # Size factors with an added pseudo-count.
-    sizeP.sf <- estimateSizeFactorsForMatrix(subcounts+1)
-                                
-    # Size factors with a library size-adjusted pseudo-count.
-    lib.size <- colSums(subcounts)
-    pcounts <- t(t(subcounts) + lib.size/mean(lib.size))
-    sizeP2.sf <- estimateSizeFactorsForMatrix(pcounts)
-}
+        # Size factors with an added pseudo-count.
+        sizeP.sf <- estimateSizeFactorsForMatrix(subcounts+1)
 
-# Library size normalization.
-lib.sf <- librarySizeFactors(counts)
+        # Size factors with a library size-adjusted pseudo-count.
+        lib.size <- colSums(subcounts)
+        pcounts <- t(t(subcounts) + lib.size/mean(lib.size))
+        sizeP2.sf <- estimateSizeFactorsForMatrix(pcounts)
+    }
 
-# Size factors with summation, no abundance filtering for simplicity.
-sizes <- seq(21, 101, by=5)
-sizes <- sizes[sizes <= ncol(counts)]
-final.sf <- computeSumFactors(counts, sizes=sizes, clusters=NULL, min.mean=threshold)
+    # Library size normalization.
+    lib.sf <- librarySizeFactors(counts)
 
-# Size factors with clustering prior to summation:
-if (ncol(counts) >= 200) {
-    emp.clusters <- quickCluster(counts, method="igraph")
-    final2.sf <- computeSumFactors(counts, sizes=sizes, clusters=emp.clusters, min.mean=threshold)
-} else {
-    final2.sf <- final.sf
-}
+    # Size factors with summation, no abundance filtering for simplicity.
+    sizes <- seq(21, 101, by=5)
+    sizes <- sizes[sizes <= ncol(counts)]
+    final.sf <- computeSumFactors(counts, sizes=sizes, clusters=NULL, min.mean=threshold)
 
-# Using a knn-based method for summation.
-final3.sf <- simpleSumFactors(counts, min.mean=threshold, approximate=TRUE)
+    # Size factors with clustering prior to summation:
+    if (ncol(counts) >= 200) {
+        emp.clusters <- quickCluster(counts, method="igraph")
+        final2.sf <- computeSumFactors(counts, sizes=sizes, clusters=emp.clusters, min.mean=threshold)
+    } else {
+        final2.sf <- final.sf
+    }
+
+    # Using a knn-based method for summation.
+    final3.sf <- simpleSumFactors(counts, min.mean=threshold, approximate=TRUE)
 
     # Reporting all methods.
     return(list(TMM=tmm.sf, TMM.ave=tmm2.sf,
